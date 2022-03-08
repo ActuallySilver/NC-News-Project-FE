4c9e4e60f069996fd0f800206f1266e2f72e038a
ActuallySilver <jason@educateit.net>
2022-03-08 15:24:05 +0000

    Added a missing / at the beginning of a path

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Articles />} />
-          <Route path="topics/:topic" element={<Articles />} />
+          <Route path="/topics/:topic" element={<Articles />} />
           <Route path="/users" element={<Users />} />
         </Routes>
       </div>
--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getArticles, getArticlesByTopic } from "../api";
+import { getArticles } from "../api";
 import ArticleCard from "./ArticleCard";
 import ArticleNav from "./ArticleNav";
 
