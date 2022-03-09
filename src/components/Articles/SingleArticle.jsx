845464e1eed896a6f421eb50023182dedfa43bfd
ActuallySilver <jason@educateit.net>
2022-03-09 17:50:52 +0000

    Created viewing a single article

---
+++ src/components/Articles/SingleArticle.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticle, getComments } from "../api";
+import getTimePassedSince from "../getTimePassedSince";
+import CommentCard from "./CommentCard";
+
+export default function SingleArticle() {
+  const { article_id } = useParams();
+  const [article, setArticle] = useState({});
+  const [articleComments, setArticleComments] = useState([]);
+  useEffect(() => {
+    getArticle(article_id).then(setArticle);
+    getComments(article_id).then(setArticleComments);
+  }, [article_id]);
+
+  return (
+    <div>
+      <h2>{article.title}</h2>
+      <div className="article-details">
+        <section className="stats">
+          <section className="article-creation-info">
+            <h5>{getTimePassedSince(article.created_at)}</h5>
+            <h4>{article.topic}</h4>
+            <h4>By {article.author}</h4>
+          </section>
+          <section className="article-stats">
+            <h4>{article.votes} votes </h4>
+            <h4>{article.comment_count} comments</h4>
+          </section>
+        </section>
+        <section className="article-body">
+          <p>{article.body}</p>
+        </section>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/getTimePassedSince.js
@@ -0,0 +1,27 @@
+const getTimePassedSince = (time) => {
+  const currentTime = Date.now();
+  const timePassed = currentTime - Date.parse(time);
+  const years = Math.floor(timePassed / 1000 / 60 / 60 / 24 / 365.25);
+  if (years !== 0) {
+    if (years === 1) return years + " year ago";
+    return years + " years ago";
+  }
+  const days = Math.floor(timePassed / 1000 / 60 / 60 / 24);
+  if (days !== 0) {
+    if (days === 1) return days + " day ago";
+    return days + " days ago";
+  }
+  const hours = Math.floor(timePassed / 1000 / 60 / 60);
+  if (hours !== 0) {
+    if (hours === 1) return hours + " hour ago";
+    return hours + " hours ago";
+  }
+  const minutes = Math.floor(timePassed / 1000 / 60);
+  if (minutes !== 0) {
+    if (minutes === 1) return minutes + " minute ago";
+    return minutes + " minutes ago";
+  }
+  return "just now";
+};
+
+export default getTimePassedSince;
