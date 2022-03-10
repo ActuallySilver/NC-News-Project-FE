81d91ed98b133c6c3e651c3393596cff71ac0be1
ActuallySilver <jason@educateit.net>
2022-03-10 17:46:25 +0000

    Completed filtering of articles

--- src/components/Articles/ArticleNav.jsx
+++ src/components/Articles/ArticleNav.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { getTopics } from "../api";
 
 export default function ArticleNav() {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [topics, setTopics] = useState([]);
+  const [orderType, setOrderType] = useState("ASC");
+  const [orderBy, setOrderBy] = useState("created_at");
+  const [showFilters, setShowFilters] = useState(false);
+
   useEffect(() => {
     getTopics().then(setTopics);
   }, []);
@@ -20,6 +25,57 @@ export default function ArticleNav() {
           </Link>
         );
       })}
+      <button
+        onClick={() => {
+          setShowFilters((currentSetting) => {
+            return !currentSetting;
+          });
+        }}
+      >
+        Filters
+      </button>
+
+      {showFilters && (
+        <section>
+          Order{" "}
+          <select
+            name="order_by"
+            id="order_by"
+            onChange={(e) => {
+              setOrderBy(e.target.value);
+            }}
+          >
+            <option value="created_at">date</option>
+            <option value="comment_count">comment count</option>
+            <option value="votes">votes</option>
+          </select>
+          {" by "}
+          <select
+            name="order_type"
+            id="order_type"
+            onChange={(e) => {
+              setOrderType(e.target.value);
+            }}
+          >
+            <option value="ASC">ascending</option>
+            <option value="DESC">decending</option>
+          </select>
+          <button
+            onClick={() => {
+              setSearchParams({ sort_by: orderBy, order: orderType });
+            }}
+          >
+            Apply
+          </button>
+          <button
+            onClick={() => {
+              setSearchParams({});
+            }}
+          >
+            Clear filters
+          </button>
+        </section>
+      )}
     </div>
   );
 }
